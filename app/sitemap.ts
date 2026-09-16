2709bf493ab840403bb7ce22997464316548843e
awais-ali-hsol <awais.ali@heuristicsol.com>
2026-09-16 15:19:13 +0500

    fix robots files

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { services } from "@/lib/data";
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: "https://tectonsolutions.com", lastModified: new Date(), changeFrequency: "monthly", priority: 1 },
